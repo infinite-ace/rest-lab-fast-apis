51ccb19dca35e1c0295517a8ebc4769d317a9922
infinite-ace <angelkukushev@infinitelambda.com>
2021-12-03 20:56:55 +0200

    Works as a charm

--- UserService/UserDTO.py
+++ UserService/UserDTO.py
@@ -1,22 +1,8 @@
+from pydantic import BaseModel
 
 
-class UserDTO:
+class UserDTO(BaseModel):
     id: int
     name: str
     age: int
     address: str
-
-    def __init__(self,id, name, age, address):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.address = address
-
-    def create(id, name, age, address):
-        user = UserDTO()
-        user.id = id
-        user.name = name
-        user.age = age
-        user.address = address
-
-        return user
--- UserService/users_service.py
+++ UserService/users_service.py
@@ -8,43 +8,48 @@ app = FastAPI()
 users_dictionary = {}
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello Users Service"}
+#   Example:
+#
+  # {
+  #   "id": 1,
+  #   "name": "Zohan",
+  #   "age": 332,
+  #   "address": "12 Main Street, NY, 60049"
+  # }
+#
+#
 
 
 @app.post("/create")
-async def create_user(request: Request):
-    request_body = await request.json()
+async def create_user(user: UserDTO):
 
-    user = UserDTO(
-                request_body['id'],
-                request_body['name'],
-                request_body['age'],
-                request_body['address']
-    )
-
-    users_dictionary[request_body['id']] = user
-    return users_dictionary
+    users_dictionary[user.id] = user
+    return user
 
 
 @app.delete("/delete")
 def delete_user(request: Request):
     request_body = request.json()
 
-    user = UserDTO(request_body['id'],
-                request_body['name'],
-                request_body['age'],
-                request_body['address'])
+    user = UserDTO.create(
+        request_body['id'],
+        request_body['name'],
+        request_body['age'],
+        request_body['address']
+    )
 
     users_dictionary.remove(user)
     return users_dictionary
 
 
-
 @app.get("/getall")
 async def get_all_users():
     return users_dictionary
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello Users Service"}
 
 
 if __name__ == "__main__":
