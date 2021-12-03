75af2a188d648c96d05a890b7c5b406ade3a88f3
infinite-ace <angelkukushev@infinitelambda.com>
2021-12-03 16:54:28 +0200

    Adds user creation with post request method body

--- UserService/User.py
+++ UserService/User.py
@@ -2,4 +2,17 @@ class User:
     name = ""
     age = 0
     favourite_book = ""
+    address = ""
 
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def create(name, age, address):
+        user = User()
+        user.name = name
+        user.age = age
+        user.address = address
+
+        return user
--- UserService/users_service.py
+++ UserService/users_service.py
@@ -1,13 +1,34 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
+
+from User import User
 
 app = FastAPI()
+
+users_dictionary = []
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello Users Service"}
 
+
+@app.post("/create")
+async def create_user(request: Request):
+    request_body = await request.json()
+
+    user = User(request_body['name'],
+                request_body['age'],
+                request_body['address'])
+
+    users_dictionary.append(user)
+    return users_dictionary
+
+
+@app.get("/getall")
+async def get_all_users():
+    return users_dictionary
+
+
 if __name__ == "__main__":
     uvicorn.run("users_service:app", host="127.0.0.1", port=8000)
-
