843fcd18212d7f3864aadcaa244cc9d2293f88cb
infinite-ace <angelkukushev@infinitelambda.com>
2021-12-07 21:49:18 +0200

    Implements CRUD for UserService

--- UserService/UserDTO.py
+++ UserService/UserDTO.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 
 class UserDTO(BaseModel):
+    id: str
     name: str
     age: int
     address: str
--- UserService/users_service.py
+++ UserService/users_service.py
@@ -13,21 +13,21 @@ users_count = 0
 
 #   Example:
 #
-  # {
-  #   "id": 1,
-  #   "name": "Zohan",
-  #   "age": 332,
-  #   "address": "12 Main Street, NY, 60049"
-  # }
+# {
+#   "id": 1,
+#   "name": "Sebastian",
+#   "age": 332,
+#   "address": "12 Main Street, NY, 60049"
+# }
 #
 #
 
 
 @app.post("/create")
 def create_user(user: UserDTO):
-
     user_uuid = str(uuid.uuid4())
-    address = user.address
+    user.id = user_uuid
+    address = user.address = user.address
 
     requests.post(f"http://127.0.0.1:8001/address?id={user_uuid}&address={address}").text
 
@@ -35,9 +35,18 @@ def create_user(user: UserDTO):
     return user
 
 
+@app.get("/read")
+def read_user(user_uuid: str):
+    return users_dictionary[user_uuid]
+
+
+@app.put("/update")
+def update_user(user: UserDTO):
+    users_dictionary[user.id] = user
+
+
 @app.delete("/delete")
 def delete_user(id: str):
-
     users_dictionary.pop(id, None)
     return users_dictionary
 
